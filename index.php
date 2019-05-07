1eea37fd7e1de5535de5593caa3dc5caf851e495
Cinara Daniele Silva Costa <48722501+cinaradaniele@users.noreply.github.com>
2019-05-07 11:48:47 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+	include("cabecalho.php");
+?>
+
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>Sobre Mim</h2>
+      <h5>Minha Foto:</h5>
+      <div class="fakeimg"><img src="minhafoto.png" width="200px" height="200px"></div>
+      <p>Estudo e trabalho no Hospital Santa Rita.</p>
+      <h3>Alguns links</h3>
+      <p>Locais de Estudo.</p>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Colégio Lisboa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Faculdade Senac</a>
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+    <div class="col-sm-8">
+      <h2>Aula de Responsividade</h2>
+      <h5>Framework Bootstrap 4.1, 16 de abril de 2019</h5>
+      <div class="fakeimg"><img src="boods.jpg" alt="" width="250px" height="200px"></div>
+      <p>Descrição..</p>
+      <p>Construção de uma página com a utilização do framework de css - boostratp na versão 4.1.</p>
+      <br>
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Sep 2, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+  </div>
+</div>
+
+<?php
+	include("rodape.php");	
+?>
